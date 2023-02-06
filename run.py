611e381f1d85172c7d39110276b0734bc7c0a351
Jamsey911 <jamesalynch911@hotmail.com>
2023-02-06 03:21:54 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,147 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" 
+Hangman Game: Computer picks word from a list and user 
+has to guess the letters in the word.
+User has 6 chances before game over.
+"""
+
+# Import Random
+import random
+
+# Constants
+HANGMAN = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+"""
+List of words for computer to randomize
+"""
+file = open('data.txt', 'r')
+WORDS = file.readlines()
+file.close()
+my_word = 'a'
+while len(my_word) < 3:
+    # makes sure word is at least 3 letters long
+    my_word = random.choice(WORDS)
+    my_word = str(my_word).strip('[]')
+    my_word = str(my_word).strip("''")
+    my_word = str(my_word).strip("\n")
+    my_word = str(my_word).strip("\r")
+    my_word = str(my_word).upper()
+    print(my_word)
+    
+
+# """
+# Lives generated for user to abide by
+# """
+
+LIVES = len(HANGMAN) - 1
+
+# Initalize variables
+
+current_guess = "-" * len(my_word)
+
+# Wrong game counter
+
+wrong_guesses = 0
+
+# used letter tracker 
+used_letters = []
+
+# Main loop
+print("Welcome to Hangman")
+print("Try to guess the word")
+
+while wrong_guesses < LIVES and current_guess != my_word:
+    print(HANGMAN[wrong_guesses])
+    print("You've used the following letters: ", used_letters)
+    print("So far, the word is:", current_guess)
+
+    guess = input("Select a letter:")
+    guess = guess.upper()
+    print(guess)
+
+# Check letter 
+while guess in used_letters:
+    print("You've already guessed that letter:", guess)
+    guess = input("Enter your guess:")
+    guess = guess.upper()
+
+
+# Add new guessed letter to list of guessed letters
+used_letters.append(guess)
+print(used_letters)
+
+if guess in my_word:
+    print("You've guessed correctly")
+
+    # Give a new version of the word mixed letters and dashs 
+
+    new_current_guess = ""
+    for letter in range(len(my_word)):
+        if guess == my_word[letter]:
+            new_current_guess += guess
+        else:
+            new_current_guess += current_guess[letter]
+
+    current_guess = new_current_guess
+else:
+    print("That was incorrect")
+
+# End of game
+if wrong_guesses == LIVES:
+    print(HANGMAN[wrong_guesses])
+    print("You've been Hanged!")
+    print("The correct word is: ", my_word)
+
+else:
+    print("You've Won!")
+
+
+print(used_letters)
