1cbcc819b4835c08f522cfb8e8113f5bb1e52b7f
Jamsey911 <jamesalynch911@hotmail.com>
2023-02-14 00:28:35 +0000

    Correct uppercase issue with repeating letters

--- run.py
+++ run.py
@@ -39,109 +39,103 @@ while len(MY_WORD) < 3:
 # """
 # Lives generated for user to abide by
 # """
-def MAIN_GAME():
-    LIVES = len(images.HANGMAN) - 1
+LIVES = len(images.HANGMAN) - 1
 
-    # Initalize variables
+# Initalize variables
 
-    current_guess = "-" * len(MY_WORD)
+current_guess = "-" * len(MY_WORD)
 
-    # Wrong game counter
+# Wrong game counter
 
-    WRONG_GUESSES = 0
+WRONG_GUESSES = 0
 
-    # used letter tracker
-    used_letters = []
+# used letter tracker
+used_letters = []
 
-    # Main loop
-    print("Welcome to Hangman")
-    print("Try to guess the word")
-    print(MY_WORD)
+# Main loop
+print("Welcome to Hangman")
+print("Try to guess the word")
+print(MY_WORD)
 
 
 # Calcaltes how many lives are left
-    while WRONG_GUESSES < LIVES and current_guess != MY_WORD:
+while WRONG_GUESSES < LIVES and current_guess != MY_WORD:
+    print(images.HANGMAN[WRONG_GUESSES])
+    print("You've used the following letters: ", used_letters)
+    print("So far, the word is:", current_guess)
+    print("You have", WRONG_GUESSES, "of 8 guesses left")
+
+    guess = input("Select a letter:\n")
+    # Checks if a single letter is entered
+    if len(guess) == 1 and guess.isalpha():
+        os.system("clear")
+        guess = guess.upper()
+    # Changes input to uppercase and checks if letter is in word
+    elif guess.upper() == MY_WORD:
+        os.system("clear")
+        break
+    # 2 lives used if a word is incorrect
+    elif len(guess) == len(MY_WORD) and guess.upper() != MY_WORD:
+        os.system("clear")
+        print(guess + " is incorrect.")
+        WRONG_GUESSES += 1
+    # Prompt for an incorrect value
+    else:
+        os.system("clear")
+        print(guess + """is an incorrect entry, please select a
+            single letter or take a guess at the word.""")
+        continue
+# Check letter
+    while guess in used_letters:
+        print("You've already guessed that letter:", guess)
         print(images.HANGMAN[WRONG_GUESSES])
         print("You've used the following letters: ", used_letters)
         print("So far, the word is:", current_guess)
-        print("You have", WRONG_GUESSES, "of 8 guesses left")
+        print("You have", WRONG_GUESSES, " of 8 guesses left")
+        guess = input("Enter your guess:\n").upper()
+        os.system("clear")
+# Add new guessed letter to list of guessed letters
+    used_letters.append(guess)
+    # print(used_letters)
 
-        guess = input("Select a letter:\n")
-        # Checks if a single letter is entered
-        if len(guess) == 1 and guess.isalpha():
-            os.system("clear")
-            guess = guess.upper()
-        # Changes input to uppercase and checks if letter is in word
-        elif guess.upper() == MY_WORD:
-            os.system("clear")
-            break
-        # 2 lives used if a word is incorrect
-        elif len(guess) == len(MY_WORD) and guess.upper() != MY_WORD:
-            os.system("clear")
-            print(guess + " is incorrect.")
-            WRONG_GUESSES += 1
-        # Prompt for an incorrect value
-        else:
-            os.system("clear")
-            print(guess + """is an incorrect entry, please select a
-                single letter or take a guess at the word.""")
-            continue
-    # Check letter
-        while guess in used_letters:
-            print("You've already guessed that letter:", guess)
-            print(images.HANGMAN[WRONG_GUESSES])
-            print("You've used the following letters: ", used_letters)
-            print("So far, the word is:", current_guess)
-            print("You have", WRONG_GUESSES, " of 8 guesses left")
-            guess = input("Enter your guess:\n")
-            os.system("clear")
-    # Add new guessed letter to list of guessed letters
-        used_letters.append(guess)
-        # print(used_letters)
+    if guess in MY_WORD:
+        print("You've guessed correctly")
 
-        if guess in MY_WORD:
-            print("You've guessed correctly")
+        # Calcalate lives to add to lives
 
-            # Calcalate lives to add to lives
+        NEW_CURRENT_GUESS = ""
+        for letter in range(len(MY_WORD)):
+            if guess == MY_WORD[letter]:
+                NEW_CURRENT_GUESS += guess
+            else:
+                NEW_CURRENT_GUESS += current_guess[letter]
 
-            NEW_CURRENT_GUESS = ""
-            for letter in range(len(MY_WORD)):
-                if guess == MY_WORD[letter]:
-                    NEW_CURRENT_GUESS += guess
-                else:
-                    NEW_CURRENT_GUESS += current_guess[letter]
-
-            current_guess = NEW_CURRENT_GUESS
-            print(NEW_CURRENT_GUESS)
-        else:
-            print("That was incorrect")
-            WRONG_GUESSES += 1
+        current_guess = NEW_CURRENT_GUESS
+        print(NEW_CURRENT_GUESS)
+    else:
+        print("That was incorrect")
+        WRONG_GUESSES += 1
 
 # End of game
-    if WRONG_GUESSES == LIVES:
-        print(images.HANGMAN[WRONG_GUESSES])
-        print("You've been Hanged!")
-        print("The correct word is: ", MY_WORD)
+if WRONG_GUESSES == LIVES:
+    print(images.HANGMAN[WRONG_GUESSES])
+    print("You've been Hanged!")
+    print("The correct word is: ", MY_WORD)
 
-    else:
-        print("You've Won! The word was", MY_WORD)
-        user_input = input(f"{PLAY_AGAIN_MSG}>>> ").lower()
-        while True:
-            if user_input == "a":
-                os.system("clear")
-                print("You have decided to continue playing the game.")
-                return MAIN_GAME()
-            elif user_input == "b":
-                print(images.game_info, "Game Rules")
-                print("Thanks for playing, Hope to see you again soon!")
-                break
-            elif user_input == "c":
-                print("Now closing the game...")
-                print("Thanks for playing, Hope to see you again soon!")
-                break
-            else:
-                print("That is not a valid option. Please try again.")
-                user_input = input(f"{PLAY_AGAIN_MSG}>>> ").lower()
-
-
-MAIN_GAME()
+else:
+    print("You've Won! The word was", MY_WORD)
+    user_input = input(f"{PLAY_AGAIN_MSG}>>> ").lower()
+    while True:
+        if user_input == "a":
+            os.system("clear")
+            print("You have decided to continue playing the game.")
+        elif user_input == "b":
+            os.system("clear")
+            print(images.game_info, "Game Rules")
+        elif user_input == "c":
+            print("Now closing the game...")
+            print("Thanks for playing, Hope to see you again soon!")
+            break
+        else:
+            print("That is not a valid option. Please try again.")
+            user_input = input(f"{PLAY_AGAIN_MSG}>>> ").lower()
