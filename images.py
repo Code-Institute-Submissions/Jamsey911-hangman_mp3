0d65d1927f52bd243f3ac42d94e76a9c3c7b5978
Jamsey911 <jamesalynch911@hotmail.com>
2023-02-15 01:40:31 +0000

    Change postion of Rules image

--- images.py
+++ images.py
@@ -67,23 +67,23 @@ _/ \_ |
 game_info = [
  # hangman game rules
  """
-_________________________________________________________________________
-|   ___________________________________________________________________   |
-|  |                                                                   |  |
-|  |                ===============================                    |  |
-|  |                | |                         | |                    |  |
-|  |                | |   G A M E   R U L E S   | |                    |  |
-|  |                | |                         | |                    |  |
-|  |                ===============================                    |  |
-|  |                                                                   |  |
-|  |  1 - You have 8 attempts to try to find the right word by         |  |
-|  |      inputting letters or the full word                           |  |
-|  |  2 - If you guess a wrong letter you will lose an attempt and the |  |
-|  |      hangman will begin building                                  |  |
-|  |  3 - When you take 8 guesses, you will be hanged - Game Over      |  |
-|  |  POINTS:                                                          |  |
-|  |  4 - You can take a guess at the word but you will be deduted 2   |  |
-|  |  guesses                                                          |  |
-|  |___________________________________________________________________|  |
-|_________________________________________________________________________|
+    ______________________________________________________________________
+   |   ________________________________________________________________   |
+   |  |                                                                |  |
+   |  |                ===============================                 |  |
+   |  |                | |                         | |                 |  |
+   |  |                | |   G A M E   R U L E S   | |                 |  |
+   |  |                | |                         | |                 |  |
+   |  |                ===============================                 |  |
+   |  |                                                                |  |
+   |  |  1 - You have 8 attempts to try to find the right word by      |  |
+   |  |      inputting letters or the full word                        |  |
+   |  |  2 - If you guess a wrong letter you will lose an attempt and  |  |
+   |  |      the hangman will begin building                           |  |
+   |  |  3 - When you take 8 guesses, you will be hanged - Game Over   |  |
+   |  |  POINTS:                                                       |  |
+   |  |  4 - You can take a guess at the word but you will be deduted  |  |
+   |  |  2 guesses                                                     |  |
+   |  |________________________________________________________________|  |
+   |______________________________________________________________________|
 """,]
